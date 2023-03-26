f21d621f75f19b809c313569b4e98777dc06bbc9
Constantin Mihailov <constantin.mhv@gmail.com>
2023-03-26 15:02:39 +0300

    Add general UI structure. Add empty navigation graph.

---
+++ KeepMeUpdatedApp/app/src/main/java/com/app/keepmeupdated/ui/home/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.app.keepmeupdated.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.app.keepmeupdated.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+}
